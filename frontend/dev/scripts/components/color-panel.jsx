e6c77cfc85b0438e741dd670d87a2ee9ccce0787
Nicolas Gouwy <43481095+Iconejey@users.noreply.github.com>
2023-01-18 19:41:22 +0100

    Color input edition

--- frontend/dev/scripts/components/color-panel.jsx
+++ frontend/dev/scripts/components/color-panel.jsx
@@ -15,15 +15,38 @@ class ColorPanel {
 			</span>
 		</This>;
 
+		// Set color
 		this.color = this.getAttribute('color') ?? '#121212';
+
+		this.input = this.querySelector('#color');
+		this.input.innerText = this.color.replace('#', '');
+
+		// Listen for color edit
+		this.input.oninput = e => {
+			let color = this.input.innerText;
+
+			// Ignore spaces
+			if (/\s/.test(color)) {
+				this.input.innerText = color.replace(/\s/g, '');
+				color = this.input.innerText;
+			}
+
+			const invalid = this.input.classList.toggle('invalid', !/^[0-9a-f]{6}$/i.test(color));
+			if (!invalid) this.color = '#' + color;
+		};
+
+		// Listen for color paste
+		this.input.onpaste = e => {
+			e.preventDefault();
+			const text = e.clipboardData.getData('text/plain');
+			this.input.innerText = text.replace(/[^0-9a-f]/gi, '');
+			this.input.dispatchEvent(new Event('input'));
+		};
 	}
 
 	set color(color) {
 		// Set panel color
 		this.setAttribute('style', `background-color: ${color};`);
-
-		// Set color text
-		this.querySelector('#color').innerText = color;
 
 		// Choose white or black text depending on panel color brightness
 		let [r, g, b] = color.match(/\w\w/g).map(x => parseInt(x, 16));
